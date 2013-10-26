0a8a5addb71523776a240777e965ba6188dbf60a
Vlad <empty>
2013-10-26 23:20:19 +0300

    Add scientific notation support and code fix

--- Cpp_cin_input_check/getvalue.cpp
+++ Cpp_cin_input_check/getvalue.cpp
@@ -1,72 +1,97 @@
 #include "getvalue.h"
 
 #include <iostream>
-#include <string>
-#include <cstdlib>
-#include <conio.h>
-
 using std::cout;    // считается, что поток вывода это обычный cout
+
+#include <string>
 using std::string;
 
-//template <typename T>
+#include <cstdlib>  // strtold
+#include <conio.h>  // getch
+
+char getLastSymbol(string *str) {
+    return *( str->rbegin() );   // strRes.back() in C++11
+}
+
+char deleteLastSymbol(string *str) {
+    char lastSymbol = getLastSymbol(str);
+    str->erase(str->length() - 1);
+    return lastSymbol;
+}
+
+// Може використати boost для переведення рядка символів до числа?..
+
 long double getDoubleVal() {
-    string strRes;          // Храниться введенное пользователем значение
-    char input = ' ';       // В этой переменной хранится символ, который вводится на каждой итерации цикла
-    int lastElementIndex = 0;   // используется при удалении символа
-    bool isDecimalSeparatorInputted = false;
-    bool isValidInput = false;
-
-
-    do {
-        input = getch();       // getch записывает введенный символ в переменную не дожидаясь нажатия ентера
-
+    string strResult;          // В цій змінній, зберігається введене користувачем значення, яке буде переведене до числа
+    char input = ' ';       // Тут зберігається введений користувачем символ
+    bool isDecimalSeparatorInputted = false;    // Флаг, чи вводив користувач десятковий роздільник
+    bool isExponentSymbolInputted = false;      //
+    bool isInputWillBeDisplayed = false;        // Флаг який означає чи буде виведено введений користувачем символ
+    //                                          // може краще використовувати continue;
+    
+    
+    while(input != '\r') {
+        input = getch();    // getch отримує один введений користувачем символ
+        //                  // та не чекаючи натиснення ентеру повертає його
+        
         switch(input) {
         case '-':
-            if(strRes == "") {
-                strRes += '-';
-                isValidInput = true;    // Для вывода введенного символа, может лучше использовать continue?
+            if(strResult == "" || getLastSymbol(&strResult) == 'e') {
+                strResult += '-';
+                isInputWillBeDisplayed = true;
             }
-            break;
-        case 13:                // Числа вводятся пока не будет нажат ентер(символ 13)
-            isValidInput = false;
             break;
         case '.':
         case ',':
             if(isDecimalSeparatorInputted == false) {
                 isDecimalSeparatorInputted = true;
-                strRes += '.';
-                isValidInput = true;
+                strResult += '.';
+                isInputWillBeDisplayed = true;
+            }
+            break;
+        case 'e':
+            if(isExponentSymbolInputted == false) {
+                isExponentSymbolInputted = true;
+                strResult += 'e';
+                isInputWillBeDisplayed = true;
             }
             break;
         case '\b':  // удаление символов
-            if(!strRes.empty()) {
-                lastElementIndex = strRes.length() - 1;
-                char lastElem = strRes.at(lastElementIndex);
-                if(lastElem == '.' || lastElem == ',') {
+            if(!strResult.empty()) {
+                char deletedSymbol = deleteLastSymbol(&strResult);
+                
+                switch(deletedSymbol) {
+                case '.':
+                case ',':
                     isDecimalSeparatorInputted = false;
+                    break;
+                case 'e':
+                    isExponentSymbolInputted = false;
+                    break;
                 }
-                strRes.erase(lastElementIndex, 1);
+                
                 cout << "\b \b";
+                isInputWillBeDisplayed = false;
             }
             break;
         default:
-            if((input >= 48 && input < 58)) {   // 47 < ch < 58 // Если введенный символ это цифра то...
-                // числа от 47 до 58 в кодовой таблице это цифры
-                strRes += input;           // Добавление введенных символов к строке для последующего распознавания
-                isValidInput = true;
+            if((input >= 48 && input < 58)) {   // 47 < ch < 58 // Якщо введений символ це цифра
+                //                              // числа від 47 до 58 в кодовій таблиці АСКІ це цифры
+                strResult += input;           // додавання введеної цифри до рядку який в подальшому буде конвертований в число
+                isInputWillBeDisplayed = true;
             }
             break;
         }
-
-        if(isValidInput == true) {
+        
+        if(isInputWillBeDisplayed == true) {
             cout << input;
-            isValidInput = false;
+            isInputWillBeDisplayed = false;
         }
-    } while(input != '\r');
-
+    }
+    
 #ifdef _MSC_VER
-    return _Stold(strRes.c_str(), NULL, 0);
+    return _Stold(strRes.c_str(), 0, 0);
 #else
-    return strtold(strRes.c_str(), NULL); // stold() ?
+    return strtold(strResult.c_str(), NULL); // stold() ?
 #endif
 }
